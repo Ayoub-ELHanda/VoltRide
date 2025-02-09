a8feba32f3488dc3a1d7e13162a5686b37ec67e6
Salim-T <tizi_salim@yahoo.fr>
2025-02-09 18:15:01 +0100

    create first models

---
+++ backend/src/domain/entities/User.ts
@@ -0,0 +1,40 @@
+export class User {
+    constructor(
+        private readonly id: string,
+        private email: string,
+        private password: string,
+        private role: string
+    ) {
+        if (role !== 'ADMIN' && role !== 'PARTNER' && role !== 'DEALER') {
+            throw new Error("Le rôle de l'utilisateur doit être 'ADMIN', 'PARTNER' ou 'DEALER'.");
+        }
+    }
+
+    getId(): string {
+        return this.id;
+    }
+
+    getEmail(): string {
+        return this.email;
+    }
+
+    setEmail(email: string): void {
+        this.email = email;
+    }
+
+    getPassword(): string {
+        return this.password;
+    }
+
+    setPassword(password: string): void {
+        this.password = password;
+    }
+
+    getRole(): string {
+        return this.role;
+    }
+
+    setRole(role: string): void {
+        this.role = role;
+    }
+}
